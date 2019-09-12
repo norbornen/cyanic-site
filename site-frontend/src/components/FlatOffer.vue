13d02f0aa8496101a470c773ffc0adb9fadab40a
Alexey Kolpakov <al.kolpak@gmail.com>
2019-09-12 03:07:18 +0300

    init

--- site-frontend/src/components/FlatOffer.vue
+++ site-frontend/src/components/FlatOffer.vue
@@ -2,34 +2,30 @@
     <div class="mb3 mb4-l mb4-m">
         <h3 class="ma0 fw5  georgia">
             <span class="dib bg-black-90 white  pa2 pa3-m pa3-l">
-                {{ offer.source.name }}: <a :href="offer.ext_full_url" target="blank" class="white">{{ offer.location.address }}</a>
+                <small class="f7 avenir db mb1">{{ new Date(offer.createdAt).toLocaleString('ru') }}</small>
+                <span>{{ offer.source.name }}: <a :href="offer.ext_full_url" target="blank" class="white">{{ offer.location.address }}</a></span>
             </span>
         </h3>
-        <div class="bg-white dt  w-100  avenir">
+        <div class="bg-white dt  w-100">
             <div class="dtc w-30 tc v-mid  pa2 pa3-m pa3-l mr2  f4 f3-l f3-m">
                 {{ offer.rooms_count }}<br/>комн
             </div>
-            <div class="dtc w-30-ns w-40 tc v-mid  pa2 pa3-m pa3-l mr2  nowrap  f4 f3-l f3-m">
+            <div class="dtc w-40 tc v-mid  pa2 pa3-m pa3-l mr2  nowrap  f4 f3-l f3-m">
                 {{ offer.price.amount }} ₽
             </div>
             <div class="dtc w-30 tc v-mid  pa2 pa3-m pa3-l mr2  f5 f4-l f4-m">
-                <span v-if="!!offer.floor_number" class="nowrap">
+                <div v-if="!!offer.floor_number" class="nowrap mb2">
                     {{ offer.floor_number }} этаж{{ !!offer.floors_total ? ` / ${offer.floors_total}` : '' }}
-                </span>
-            </div>
-            <div class="ys  dn dtc-ns w-10 tc v-mid  pv2 pv3-m pv3-l">
-                 <yandex-share :services="['telegram','whatsapp','viber']" :url="offer.ext_full_url" vertical small />
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import YandexShare from '@cookieseater/vue-yandex-share';
 export default {
     name: 'FlatOffer',
     components: {
-        YandexShare
     },
     props: {
         offer: Object
@@ -38,12 +34,7 @@ export default {
 </script>
 
 <style scoped>
-    .ys >>> .ya-share2__badge {
-        filter: grayscale(100);
-        opacity: .25;
-    }
-    .ys >>> .ya-share2__title {
-        display: none;
-        visibility: hidden;
+    .created-at {
+        font-size: 0.75rem;
     }
 </style>
